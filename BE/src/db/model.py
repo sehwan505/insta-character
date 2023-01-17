561df6022524204942d09161b08cb3c8f4d53ed2
sehwan505 <sehwan505@gmail.com>
2023-01-17 14:47:29 +0000

    add media model

--- BE/src/db/model.py
+++ BE/src/db/model.py
@@ -10,10 +10,25 @@ class User(Base):
     __tablename__ = "user"
 
     id = Column(BIGINT, nullable=False, autoincrement=True, primary_key=True)
-    insta_id = Column(TEXT, nullable=True)
+    insta_id = Column(TEXT, primary_key=True)
     name = Column(TEXT, nullable=True)
     followers_count = Column(INT, nullable=True)
     follows_count = Column(INT, nullable=True)
     biography = Column(TEXT, nullable=True)
     updated_at = Column(TIMESTAMP)
 
+
+class InstaMedia(Base):
+    __tablename__ = "media"
+
+    id = Column(BIGINT, nullable=False, autoincrement=True, primary_key=True)
+    insta_id = Column(TEXT, ForeignKey('user.media'))
+    comments_count = Column(INT, nullable=True)
+    like_count = Column(INT, nullable=True)
+    media_url = Column(TEXT, nullable=True)
+    caption = Column(TEXT, nullable=True)
+    media_type = Column(TEXT, nullable=True)
+    media_product_type = Column(TEXT, nullable=True)
+    user = relationship("user")
+
+# create table InstaMedia in mysql
